c811be7252cac73d18788df035881ba093fd1737
jyates <jared.yates@gmail.com>
2014-01-21 17:24:55 +1300

    Ported up [2/2] Settings: Slims Hardware Key Rebinding
    First thanks to CM team for the 2 years where we used their approach for our
    custom hw key rebinding. This hardware key rebinding
    patches did in the past a good job and we extended it prior 4.3 with a lot
    Slim custom stuff.
    
    But it is time to go new way and to do the complete
    hardware key rebinding more flexible and more centralized. Especially due
    that we have now all possible shorcutactions centralized in 4.4
    
    Possible mapping are:
    ACTION_HOME
    ACTION_BACK
    ACTION_SEARCH (in app search)
    ACTION_VOICE_SEARCH
    ACTION_MENU_BIG
    ACTION_POWER
    ACTION_NOTIFICATIONS
    ACTION_QS
    ACTION_RECENTS
    ACTION_SCREENSHOT
    ACTION_IME
    ACTION_LAST_APP
    ACTION_KILL
    ACTION_ASSIST
    ACTION_VIB
    ACTION_SILENT
    ACTION_VIB_SILENT
    ACTION_POWER_MENU
    ACTION_TORCH
    ACTION_EXPANDED_DESKTOP
    ACTION_THEME_SWITCH
    
    and custom app/shorcut of course
    
    Possible actions on all keys are:
    normal tap / longpress / double tap
    
    So to sumarize if it is in settings array enabled user can bind whatever of the above
    actions to every hardware key action.
    
    Main changes to prior solutions are:
    - do not depend anymore on hardcoded integer values. We use now strings. This has
      the advantage that we can save whatever shorcut or intent URI we have into this
      value. And gives us the possibility in future without changing anything on the hw
      key rebinding code to get more options/or modify the options. Actions are holded
      like on all other of our features in ButtonsConstants.java
    - All actions are peformed over our SlimActions like all other features. SlimActions
      takes care about all possible solutions the user can run into and sets the right
      open animation, keyguard handling etc etc. As well it is easier to maintain only
      one code base.
    - Only observe and update the values on hardware key devices. On non hw key devices
      the complete code is non operational
    - Removed in settings the not needed overlay due that we can use the fb one to
      check if it is a hw key device or not.
    - Major cleanup of a lot old code.
    
    This patch add as well one new SlimActions action....VOICE_SEARCH
    
    To configure your device you need to add the device overlay as usual:
    
    overlay/frameworks/base/core/res/res/values/config.xml
    <!-- Hardware "softkeys" present on the device, stored as a bit field.
             This integer should equal the sum of the corresponding value for each
             of the following keys present:
                1 - Home
                2 - Back
                4 - Menu
                8 - Assistant (search)
               16 - App switch
            For example, a device with Home, Back and Menu keys would set this
            config to 7. -->
        <integer name="config_deviceHardwareKeys">7</integer>
    
    Also requires fb commit.
    
    Cheers HCP and kufi
    
    Changes to prior patchsets:
    - reflect fb changes
    - simplify code
    - code cleanup
    - instance handling of error dialog
    - some minor tweaks
    
    PS:
    - complete reworked....using now like on navbar etc custom dialogs to be
      consistent and pretty :D
    - reset to default is not implemented yet...no idea if we want that or not. Any opinions on it?
    
    PS:
    - implement fb changes and use central HwKeyHelper class
    
    PS:
    - added reset button
    
    PS:
    - added double tap to all bindable keys
    
    Change-Id: Ie4794073236f0b6fe308b931d7d6e9d02a1ddf02
    Signed-off-by: Martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	res/values/config.xml
    	res/values/slim_arrays.xml
    	res/values/slim_strings.xml
    	res/xml/slim_navigation_settings.xml
    	src/com/android/settings/slim/NavigationSettings.java
    
    Remove nav key settings from HW key rebinding commits for devices
    that have soft keys.
    
    Use correct reset icon in hardware key settings
    
    Change-Id: Ie3ef3194930dce38ee05332f5b4d402aae37ad5c
    
    Conflicts:
    	res/values/slim_arrays.xml
    	res/xml/thevoid_settings.xml
    	src/com/android/settings/validus/WolvesDenSettings.java
    
    Conflicts:
    	res/xml/wolvesden.xml
    	src/com/android/settings/candy/PrivacySettings.java

--- src/com/android/settings/zephyr/NavigationBarSettings.java
+++ src/com/android/settings/zephyr/NavigationBarSettings.java
@@ -18,13 +18,13 @@ import com.android.settings.SettingsPreferenceFragment;
 
 public class NavigationBarSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
-
+    private static final String KEY_HARDWARE_KEYS = "hardwarekeys_settings";
     private static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
     private static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
 
     private SwitchPreference mRecentsClearAll;
     private ListPreference mRecentsClearAllLocation;
-		
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +32,14 @@ public class NavigationBarSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.navigation_bar_settings);
 	PreferenceScreen prefSet = getPreferenceScreen();
 	ContentResolver resolver = getActivity().getContentResolver();
+
+       // Hide Hardware Keys menu if device doesn't have any
+        PreferenceScreen hardwareKeys = (PreferenceScreen) findPreference(KEY_HARDWARE_KEYS);
+        int deviceKeys = getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareKeys);
+        if (deviceKeys == 0 && hardwareKeys != null) {
+            getPreferenceScreen().removePreference(hardwareKeys);
+        }
 
 	mRecentsClearAll = (SwitchPreference) prefSet.findPreference(SHOW_CLEAR_ALL_RECENTS);
         mRecentsClearAll.setChecked(Settings.System.getIntForUser(resolver,
