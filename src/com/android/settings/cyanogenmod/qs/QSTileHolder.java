a972dd00751279868e0a0de1ce37db84ce6b3abe
Altaf-Mahdi <altaf.mahdi@gmail.com>
2016-01-01 03:10:50 -0500

    Add Battery Saver tile

--- src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
+++ src/com/android/settings/cyanogenmod/qs/QSTileHolder.java
@@ -143,6 +143,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_sync_on";
                 stringId = R.string.qs_tile_sync;
                 break;
+            case QSConstants.TILE_BATTERY_SAVER:
+                resourceName = "ic_qs_battery_saver_on";
+                stringId = R.string.qs_battery_saver_tile;
+                break;
             default:
                 return null;
         }
