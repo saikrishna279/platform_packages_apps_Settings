4289e8f83004ce5ade1bf90235447abc46003452
saikrishna279 <androidwall.nrt@gmail.com>
2016-08-05 16:31:43 +0530

    derp 5

--- src/com/android/settings/zephyr/AdvancedScreenDisplaySettings.java
+++ src/com/android/settings/zephyr/AdvancedScreenDisplaySettings.java
@@ -1,18 +1,3 @@
-/* Copyright (C) 2016 Sai Krishna <androidwall.nrt@gmail.com>
-
-     Licensed under the Apache License, Version 2.0 (the "License");
-     you may not use this file except in compliance with the License.
-     You may obtain a copy of the License at
-
-          http://www.apache.org/licenses/LICENSE-2.0
-
-     Unless required by applicable law or agreed to in writing, software
-     distributed under the License is distributed on an "AS IS" BASIS,
-     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     See the License for the specific language governing permissions and
-     limitations under the License.
-*/
-
 package com.android.settings.zephyr;
 
 import android.os.Bundle;
@@ -37,10 +22,8 @@ public class AdvancedScreenDisplaySettings extends SettingsPreferenceFragment im
     @Override
     public void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
-        //shit gets initialized with exisitng or NULL value!
 
 	addPreferencesFromResource(R.xml.advanced_screen_display_settings);
-        //opens the shitting fragment!
    }
 
     @Override
@@ -56,6 +39,6 @@ public class AdvancedScreenDisplaySettings extends SettingsPreferenceFragment im
 
     @Override
     protected int getMetricsCategory() {
-        return MetricsLogger.STATUS_BAR_SETTINGS;
+        return MetricsLogger.SCREEN_RESOLUTION_SETTINGS;
     }
 }
