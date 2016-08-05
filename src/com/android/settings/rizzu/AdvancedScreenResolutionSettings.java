5fbe8b0f012db166245b33de4cdbcbb792d4176e
saikrishna279 <androidwall.nrt@gmail.com>
2016-08-05 19:51:56 +0530

    Oh fuck! derp, this is the one!

--- src/com/android/settings/rizzu/AdvancedScreenResolutionSettings.java
+++ src/com/android/settings/rizzu/AdvancedScreenResolutionSettings.java
@@ -23,6 +23,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.IPackageDataObserver;
+import android.text.Spannable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
