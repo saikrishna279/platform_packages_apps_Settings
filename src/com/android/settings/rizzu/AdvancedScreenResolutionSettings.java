b93ac81e7d9aae5642e2fafdc871163dc2357c09
saikrishna279 <androidwall.nrt@gmail.com>
2016-08-05 17:45:37 +0530

    derp

--- src/com/android/settings/rizzu/AdvancedScreenResolutionSettings.java
+++ src/com/android/settings/rizzu/AdvancedScreenResolutionSettings.java
@@ -35,6 +35,7 @@ import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
+import android.preference.Preference.OnPreferenceChangeListener;
 
 import android.text.Editable;
 import android.util.Log;
