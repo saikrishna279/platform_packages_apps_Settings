6acda7289943ae1caaf10dbe1e9065f4d52fd1d0
saikrishna279 <androidwall.nrt@gmail.com>
2016-08-06 15:58:40 +0530

    Disable build.prop check for now!

--- src/com/android/settings/rizzu/AdvancedScreenResolutionSettings.java
+++ src/com/android/settings/rizzu/AdvancedScreenResolutionSettings.java
@@ -1,21 +1,4 @@
-/*   Copyright (C) 2016 Sai Krishna <androidwall.nrt@gmail.com>
-
-     Licensed under the Apache License, Version 2.0 (the "License");
-     you may not use this file except in compliance with the License.
-     You may obtain a copy of the License at
-
-          http://www.apache.org/licenses/LICENSE-2.0
-
-     Unless required by applicable law or agreed to in writing, software
-     distributed under the License is distributed on an "AS IS" BASIS,
-     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     See the License for the specific language governing permissions and
-     limitations under the License.
-*/
-
 package com.settings.android.settings.rizzu;
-
-//import necssary shit for data entry
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.ComponentName;
@@ -36,7 +19,6 @@ import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
 import android.preference.Preference.OnPreferenceChangeListener;
-
 import android.text.Editable;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -50,10 +32,7 @@ import com.android.settings.rizzu.CommandResult;
 import com.android.settings.rizzu.Helpers;
 import com.android.settings.rizzu.AbstractAsyncSuCMDProcessor;
 
-//imports necessary shit!
 import android.os.SystemProperties;
-
-//import settings provider
 import android.provider.Settings;
 import com.android.settings.R;
 
@@ -78,8 +57,8 @@ public class AdvancedScreenResolutionSettings extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.advanced_screen_resolution_settings);
 
         //imports stock screen resolution from build.prop
-        String currentResolution = SystemProperties.get("ro.wm.screen_res");
-        String resetResolution = SystemProperties.get("ro.wm.screen_res");
+        //String currentResolution = SystemProperties.get("ro.wm.screen_res");
+        //String resetResolution = SystemProperties.get("ro.wm.screen_res");
 
         AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
         alert.setTitle(R.string.stream_in_screen_resolution);
